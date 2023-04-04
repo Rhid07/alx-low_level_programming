c25c4b4e43f1e3377262e756c53274336ab2b01e
Rhid07 <opeyemidurodola007@gmail.com>
2023-04-03 18:10:10 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,23 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - print all the element in listint_s list
+ * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- * 
- * Return: number of node
+ *
+ * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-	while (h)
+
+	while (h != NULL)
 	{
-
-	printf("%d\n", h->n);
-	num++;
-	h = h->next;
-
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
 	}
-	return(num);
 
+	return (num);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
 #ifndef LISTS_H
-#define LIST_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,13 +9,12 @@
  * @n: integer
  * @next: points to the next node
  *
- * Description: singly linked list node structure
- * 
+ * Description: singly linked list node structure alx project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -33,6 +32,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-
 
 #endif
