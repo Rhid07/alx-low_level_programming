b079979e356ab2b08072766470f7b0399b001c91
Rhid07 <opeyemidurodola007@gmail.com>
2023-04-03 18:25:06 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+	if (h == NULL)
+		return(0);
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
