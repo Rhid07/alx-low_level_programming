3193aca7685dfb2357a2401d5f7ce2fe67e6534c
Rhid07 <opeyemidurodola007@gmail.com>
2023-04-10 06:02:40 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,8 +1,6 @@
 #ifndef BIT_MANIPULATION
 #define BIT_MANIPULATION
 
-#include <math.h>
-#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -12,5 +10,6 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+int _atoi(const char *s);
 #endif
 
