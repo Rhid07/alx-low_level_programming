d7cfa3737ba4bf57242bb0076284667ce5676c76
rhid07 <opeyemidurodola007@gmail.com>
2023-09-04 01:11:30 +0100

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - funcion to converts binary to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int decimal_val = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal_val = 2 * decimal_val + (b[i] - '0');
+	}
+	return (decimal_val);
+}
