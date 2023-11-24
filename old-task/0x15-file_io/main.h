34ad54a7422fab7d4ec906714c03b64754976173
Rhid07 <opeyemidurodola007@gmail.com>
2023-11-24 03:07:17 +0100

    0x14-bit_manipulation

---
+++ old-task/0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef RHID07_H
+#define RHID07_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
